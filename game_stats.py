f465eecbbbd216d7ec9a792d2bbf49b6d8e75984
llx <2982140642@qq.com>
2024-04-15 14:34:19 +0800

    外星人代码2.0

--- game_stats.py
+++ game_stats.py
@@ -1,12 +1,25 @@
-class GameStats :
-    """跟踪游戏的统计系统"""
+# coding=utf-8
 
-    def __init__(self,ai_game):
+class GameStats():
+    """跟踪游戏统计信息"""
+
+    def __init__(self, ai_settings):
         """初始化统计信息"""
-        self.settings = ai_game.settings 
+        self.ai_settings = ai_settings
         self.reset_stats()
-    
+
+        # 让游戏一开始处于非活动状态
+        self.game_active = False
+
+        # 任何情况下都不应重置最高得分
+        self.high_score = 0
+
+        # 显示等级
+        self.level = 1
+
     def reset_stats(self):
-        """初始化游戏运行期间可能变化的统计信息"""
-        self.ships_left = self.settings.ships_limit
-        
+        """初始化在游戏运行期间可能变化的统计信息"""
+        self.ship_left = self.ai_settings.ship_limit
+        # 为每次在开始游戏时都重置得分，我们在reset_stats（）而不是__init__中初始化score
+        self.score = 0
+
